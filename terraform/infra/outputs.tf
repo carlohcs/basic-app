7b4efa6b7f0912f4336bba0cc948eff13cfb83f4
carlohcs <carlohcs@gmail.com>
2024-09-08 01:36:10 -0300

    chore: Update Terraform outputs.tf to include additional app information

--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -2,14 +2,22 @@ output "APP_NAME" {
   value = kubernetes_service.basic_app_service.metadata[0].name
 }
 
-output "APP_IP" {
-  value = [for svc in data.kubernetes_service.basic_app_service_data : svc.status.load_balancer.ingress[0].ip]
+output "LOAD_BALANCER" {
+  value = data.kubernetes_service.basic_app_service_data.status[0].load_balancer[0]
 }
 
-output "APP_URL" {
-  value = [for svc in data.kubernetes_service.basic_app_service_data : svc.status.load_balancer.ingress[0].hostname]
+output "INGRESS" {
+  value = kubernetes_ingress_v1.basic_app_ingress
 }
 
-output "APP_PORT" {
-  value = [for svc in data.kubernetes_service.basic_app_service_data : svc.spec[0].port[0].port]
-}
+# output "APP_IP" {
+#   value = data.kubernetes_service.basic_app_service_data[0].status.load_balancer.ingress[0].ip
+# }
+
+# output "APP_URL" {
+#   value = data.kubernetes_service.basic_app_service_data[0].status.load_balancer.ingress[0].hostname
+# }
+
+# output "APP_PORT" {
+#   value = [for svc in data.kubernetes_service.basic_app_service_data : svc.spec[0].port[0].port]
+# }
