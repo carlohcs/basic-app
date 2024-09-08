672f3ad409e2c40545789598d13c1d9ecee411ff
carlohcs <carlohcs@gmail.com>
2024-09-08 15:40:27 -0300

    fix: add src folder to express serving

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static("./src"))
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
